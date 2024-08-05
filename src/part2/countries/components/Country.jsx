590225e1aabdb9350a273592144d4e3607b6fd62
Aqsa Umair <hafizaaqsa42@gmail.com>
2024-08-05 22:29:14 +0500

    remove debugger

--- src/part2/countries/components/Country.jsx
+++ src/part2/countries/components/Country.jsx
@@ -15,7 +15,6 @@ const Country = ({ countries }) => {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${API_KEY}&units=metric`
       );
-      debugger
       setWeather(response.data);
     } catch (error) {
       console.error('Error fetching weather data:', error);
